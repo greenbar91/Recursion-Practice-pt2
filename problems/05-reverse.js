57e63e4b5e7cd13f65995ffa202f03b1523089d2
Jacob <jacob.dietz1@g.austincc.edu>
2023-09-20 21:35:48 -0500

    05

--- problems/05-reverse.js
+++ problems/05-reverse.js
@@ -4,19 +4,27 @@ it reversed.
 
 Examples:
 
-reverse("house"); // "esuoh"
-reverse("dog"); // "god"
-reverse("atom"); // "mota"
-reverse("q"); // "q"
-reverse("id"); // "di"
-reverse(""); // ""
 ***********************************************************************/
 
 
 function reverse(str) {
-  // Your code here 
+
+  if(str.length <= 1){
+    return str;
+  }
+
+
+
+return  reverse(str.slice(1)) + str[0]
+
 }
 
+console.log(reverse("house")); // "esuoh"
+console.log(reverse("dog")); // "god"
+console.log(reverse("atom")); // "mota"
+console.log(reverse("q")); // "q"
+console.log(reverse("id")); // "di"
+console.log(reverse("")); // ""
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
 try {
